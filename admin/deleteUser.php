166886267fe995a16cf905cdcac1365b74d1ee5d
Jmrtamano124 <78140895+Jmrtamano124@users.noreply.github.com>
2023-12-05 09:49:24 +0800

    Update deleteUser.php

--- admin/deleteUser.php
+++ admin/deleteUser.php
@@ -3,7 +3,7 @@ define('LOGIN', 1);
 require ('../boot.php');
 
 $userid = $_GET['user'];
-$delteUser = $pdo->query("DELETE FROM users_accsount WHERE account_id ='$userid'");
+$delteUser = $pdo->query("DELETE FROM users_account WHERE account_id ='$userid'");
 
 $_SESSION['page'] ='Users';
 header("location:actions.php?do=success");
